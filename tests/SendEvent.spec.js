9680ce99b85b4a48fb2d583f8022d8f16e5fad1b
Yorkie Liu <yorkiefixer@gmail.com>
2017-10-02 13:35:12 +0800

    add some tests for SendEvent

--- tests/SendEvent.spec.js
+++ tests/SendEvent.spec.js
@@ -4,7 +4,35 @@ const test = require('ava');
 const SendEvent = require('../lib/SendEvent');
 const appId = 'R4AB842832E84BBD8B2DD6537DAFF790';
 
-test.cb('SendEvent.constructor', (t) => {
+test('SendEvent throws if no config', (t) => {
+  const err = t.throws(() => {
+    new SendEvent(process.env.EVENT_REQUEST_HOST, {
+      // Empty object
+    });
+  }, TypeError);
+  t.is(err.message, '"key" is required for SendEvent');
+});
+
+test('SendEvent throws if no config.secret', (t) => {
+  const err = t.throws(() => {
+    new SendEvent(process.env.EVENT_REQUEST_HOST, {
+      key: process.env.ROKID_KEY
+    });
+  }, TypeError);
+  t.is(err.message, '"secret" is required for SendEvent');
+});
+
+test('SendEvent throws if no config.device_type_id', (t) => {
+  const err = t.throws(() => {
+    new SendEvent(process.env.EVENT_REQUEST_HOST, {
+      key: process.env.ROKID_KEY,
+      secret: process.env.ROKID_SECRET,
+    });
+  }, TypeError);
+  t.is(err.message, '"device_type_id" is required for SendEvent');
+});
+
+test.cb('SendEvent constructor', (t) => {
   const sendEvent = new SendEvent(process.env.EVENT_REQUEST_HOST, {
     key: process.env.ROKID_KEY,
     secret: process.env.ROKID_SECRET,
