c246bd9a6688cadcea2f8e4d36d898c33d0ec274
Yorkie Liu <yorkiefixer@gmail.com>
2017-10-02 16:06:17 +0800

    add unhandled directive

--- tests/cloud/Common.spec.js
+++ tests/cloud/Common.spec.js
@@ -31,3 +31,37 @@ test.cb('exit app', (t) => {
     'version': '2.0.0'
   });
 });
+
+test.cb('unhandled directive', (t) => {
+  t.plan(2);
+  const client = new CloudAppEngine({
+    host            : process.env.EVENT_REQUEST_HOST,
+    key             : process.env.ROKID_KEY,
+    secret          : process.env.ROKID_SECRET,
+    device_type_id  : process.env.ROKID_DEVICE_TYPE_ID,
+    device_id       : process.env.ROKID_DEVICE_ID,
+  });
+  client.on('error', (err) => {
+    t.is(err.code, 'UNKNOWN_DIRECTIVE');
+  });
+  client.on('exit', (id) => {
+    t.is(appId, id);
+    t.end();
+  });
+  client.eval({
+    'appId': appId,
+    'response': {
+      'action': {
+        'version': '2.0.0',
+        'type': 'NORMAL',
+        'form': 'cut',
+        'shouldEndSession': true,
+        'directives': [{
+          'type': 'unhandle_type'
+        }]
+      }
+    },
+    'startWithActiveWord': false,
+    'version': '2.0.0'
+  });
+});
